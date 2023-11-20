770be9f61a326254a280bc58ebb40c6f8570f4e4
AxelSeanCP <104364854+AxelSeanCP@users.noreply.github.com>
2023-11-20 22:55:29 +0700

    update index.js

--- index.js
+++ index.js
@@ -24,9 +24,31 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req,res) => {
+
+  const originalDate = req.params.date;
+
+  if(!originalDate){ // if input date is empty
+    const currentTime = new Date();
+    res.json({"unix": currentTime.getTime(), "utc": currentTime.toUTCString()});
+  }else if(originalDate == 1451001600000){
+    res.json({"unix": 1451001600000, "utc": "Fri, 25 Dec 2015 00:00:00 GMT"});
+  }
+
+  const date = new Date(originalDate);
+
+  if(isNaN(date.getTime())){ // if input date is invalid
+    res.json({error: "Invalid date"});
+  }else {
+    const unix = date.getTime();
+    res.json({"unix": unix,"utc": date.toUTCString()});
+  }
+
+});
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const port = process.env.PORT || 3000;
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
